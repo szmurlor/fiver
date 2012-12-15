824dc9c109f91e0845f380c545e5083b320c7033
kosko.piotr <kosko.piotr@f3fdd263-7064-37e0-4400-c306c3018cec>
2012-12-15 22:49:54 +0000

    Poprawione wyświetlanie siatek XML.

--- trunk/src/fvmeshdraw.cpp
+++ trunk/src/fvmeshdraw.cpp
@@ -1,12 +1,3 @@
-//
-// C++ Implementation: fvgrid
-//
-// Description:
-//
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
 #include "fvmeshdraw.h"
 #include "fviewer.h"
 #include "fivermain.h"
@@ -15,7 +6,6 @@
 #include <attrmodel.h>
 #include <attr.h>
 #include "setofint.h"
-#include <elemlist.h>
 
 #include <QtDebug>
 #include <QMenu>
@@ -76,13 +66,13 @@ void FVMeshDraw::paintGL()
 //                    getCurrentViewer()->setSceneRadius( td );
 //                }
 //        }
-//        if (getAttrValue( tr("Transparent") ) == tr("Yes") ) {
-//                glEnable( GL_BLEND );
-//                glEnable( GL_ALPHA_TEST );
-//                //glDisable(GL_DEPTH_TEST);
-//                glAlphaFunc( GL_LESS, 1.f );
-//                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-//        }
+        if (getAttrValue( tr("Transparent") ) == tr("Yes") ) {
+                glEnable( GL_BLEND );
+                glEnable( GL_ALPHA_TEST );
+                //glDisable(GL_DEPTH_TEST);
+                glAlphaFunc( GL_LESS, 1.f );
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        }
 
         if (mesh != 0) {
                 initSubColors();
@@ -104,56 +94,55 @@ void FVMeshDraw::paintGL()
 //                if ( paintMode == "Subdomain wireframe" )
 //                        drawSubdomainWireframe();
 
-//                glDisable(GL_BLEND);
-//                glDisable(GL_LIGHTING);
+                glDisable(GL_BLEND);
+                glDisable(GL_LIGHTING);
 
-//                glColor4f(0.0f, 0.0f,0.0f,1.0f);
-//                glDisable(GL_DEPTH_TEST);
-////                if ( getAttrValue(tr("Show Elems Nums")) == "Yes" ) {
-////                        paintElemsNums();
-////                }
-////                if ( getAttrValue(tr("Show Verts Nums")) == "Yes" ) {
-////                        paintVertsNums();
-////                }
+                glColor4f(0.0f, 0.0f,0.0f,1.0f);
+                glDisable(GL_DEPTH_TEST);
+                if ( getAttrValue(tr("Show Elems Nums")) == "Yes" ) {
+                        paintElemsNums();
+                }
+                if ( getAttrValue(tr("Show Verts Nums")) == "Yes" ) {
+                        paintVertsNums();
+                }
         }
 
 
-//        glEnable(GL_DEPTH_TEST);
-//        glDisable( GL_ALPHA_TEST );
-//        glDisable(GL_BLEND);
-//        glEnable(GL_LIGHTING);
+        glEnable(GL_DEPTH_TEST);
+        glDisable( GL_ALPHA_TEST );
+        glDisable(GL_BLEND);
+        glEnable(GL_LIGHTING);
 
         fvlist->end();
 }
 
 void FVMeshDraw::paintElemsNums( )
 {
-//        SetOfInt visEle( getAttrValue(tr("Interesting Elements")), 1, grid->_elems.size() );
+        SetOfInt visEle( getAttrValue(tr("Interesting Elements")), 1, mesh->num_entities(3) );
 
-//        if (visEle.sum() > 1000) {
-//            if (QMessageBox::question( 0, tr("To many objects warning."), tr("You are trying to draw over 1000 element numbers. This process may take a long time. Do you want to continue?"), QMessageBox::Yes | QMessageBox::No ) == QMessageBox::No) {
-//                setAttrValue(tr("Show Elems Nums"), "No" );
-//                return;
-//            }
-//        }
-//        for ( int e = 0; e < (int) grid->_elems.size(); e++) {
-//                Elem * el = grid->_elems[ e ];
-//                if( ! subVisible[ el->subdomain() ] )
-//                        continue;
-//                if( ! visEle.find(e+1) )
-//                        continue;
-//                float ec[3];
-//                ec[0]= ec[1]= ec[2]= 0.0;
-//                int nNodes= el->_nodes.size();
-//                for ( int n=0; n < nNodes; n++) {
-//                        int v= el->_nodes[n];
-//                        for( int d= 0; d < 3; d++ )
-//                                ec[d] += grid->_nodes[v-1]._Coords[d];
-//                }
-//                for( int d= 0; d < 3; d++ )
-//                        ec[d] /= nNodes;
-//                getCurrentViewer()->fvRenderText(ec[0], ec[1], ec[2], QString::number( e+1 ) );
-//        }
+        if (visEle.sum() > 1000) {
+            if (QMessageBox::question( 0, tr("To many objects warning."), tr("You are trying to draw over 1000 element numbers. This process may take a long time. Do you want to continue?"), QMessageBox::Yes | QMessageBox::No ) == QMessageBox::No) {
+                setAttrValue(tr("Show Elems Nums"), "No" );
+                return;
+            }
+        }
+        dolfin::MeshConnectivity con = mesh->topology()(3,0);
+        const uint* connList = con();
+        for ( int e = 0; e < con.size(); e+=4 ) {
+                if( ! visEle.find(e/4+1) )
+                        continue;
+                float ec[3];
+                ec[0]= ec[1]= ec[2]= 0.0;
+                int nNodes= 4;
+                for ( int n=0; n < nNodes; n++) {
+                        int v= connList[e+n];
+                        for( int d= 0; d < 3; d++ )
+                            ec[d] += mesh->geometry().point(v).coordinates()[d];
+                }
+                for( int d= 0; d < 3; d++ )
+                        ec[d] /= nNodes;
+                getCurrentViewer()->fvRenderText(ec[0], ec[1], ec[2], QString::number( e/4+1 ) );
+        }
 }
 
 
@@ -314,110 +303,48 @@ void FVMeshDraw::drawNormal(QString & paintMode, double dShrink)
                 glBegin( GL_TRIANGLES );
         }
 
-        dolfin::MeshConnectivity con = mesh->topology()(2,0);
+        dolfin::MeshConnectivity con = mesh->topology()(3,0);
 //        std::cout << std::endl << con.str(true) << std::endl;
 //        std::cout << std::endl << mesh->topology()(3,0).str(true) << std::endl;
         const uint* connList = con();
 
-        for (i = 0; i < (int) con.size(); i+=3) {
-//            std::cout << connList[i] << " ";
-            dolfin::Point points[3];
+        for (i = 0; i < (int) con.size(); i+=4) {
+            if (visEle.find(i/4 + 1)){
+                //ustawienie koloru wyświetlania
+                glColor4f((GLfloat) 85/255, (GLfloat) 170/255, (GLfloat) 255/255, fTransparency);
+                //pobranie punktów czworościanu
+                dolfin::Point points[4];
+                double n[3];
+                points[0] = mesh->geometry().point(connList[i]);
+                points[1] = mesh->geometry().point(connList[i+1]);
+                points[2] = mesh->geometry().point(connList[i+2]);
+                points[3] = mesh->geometry().point(connList[i+3]);
 
-            points[0] = mesh->geometry().point(connList[i]);
-            points[1] = mesh->geometry().point(connList[i+1]);
-            points[2] = mesh->geometry().point(connList[i+2]);
+                //wyświetlanie każdej ze ścian
+                        normalny4p(points[0].coordinates(), points[1].coordinates(), points[2].coordinates(), points[3].coordinates(), n);
+                        glNormal3f( n[0], n[1], n[2] );
+                        glVertex3f( points[0].x(), points[0].y(), points[0].z() );
+                        glVertex3f( points[1].x(), points[1].y(), points[1].z() );
+                        glVertex3f( points[2].x(), points[2].y(), points[2].z() );
 
-            double Ax = points[0].x() - points[1].x();
-            double Ay = points[0].y() - points[1].y();
-            double Az = points[0].z() - points[1].z();
+                        normalny4p(points[1].coordinates(), points[2].coordinates(), points[3].coordinates(), points[0].coordinates(), n);
+                        glNormal3f( n[0], n[1], n[2] );
+                        glVertex3f( points[1].x(), points[1].y(), points[1].z() );
+                        glVertex3f( points[2].x(), points[2].y(), points[2].z() );
+                        glVertex3f( points[3].x(), points[3].y(), points[3].z() );
 
-            double Bx = points[2].x() - points[1].x();
-            double By = points[2].y() - points[1].y();
-            double Bz = points[2].z() - points[1].z();
+                        normalny4p(points[2].coordinates(), points[3].coordinates(), points[0].coordinates(), points[1].coordinates(), n);
+                        glNormal3f( n[0], n[1], n[2] );
+                        glVertex3f( points[2].x(), points[2].y(), points[2].z() );
+                        glVertex3f( points[3].x(), points[3].y(), points[3].z() );
+                        glVertex3f( points[0].x(), points[0].y(), points[0].z() );
 
-            double Nx = Ay*Bz - By*Az;
-            double Ny = Bx*Az - Ax*Bz;
-            double Nz = Ax*By - Bx*Ay;
-
-            glColor4f((GLfloat) 85/255,
-                      (GLfloat) 170/255,
-                      (GLfloat) 255/255,
-                      fTransparency);
-
-            glNormal3f( Nx, Ny, Nz );
-            glVertex3f( points[0].x(), points[0].y(), points[0].z() );
-            glVertex3f( points[1].x(), points[1].y(), points[1].z() );
-            glVertex3f( points[2].x(), points[2].y(), points[2].z() );
-
-
-//////////////////////////////////////
-//            for (uint e = 0; e < con.num_entities; e++)
-//            {
-//              s << "  " << e << ":";
-//              for (uint i = con.offsets[e]; i < con.offsets[e + 1]; i++)
-//                s << " " << con.connections[i];
-//              s << std::endl;
-//            }
-
-
-//            std::cout << i << std::endl;
-//////////////////////////////////////
-
-//                Elem * e;
-//                        //Node * n;
-//                int ie;
-//                double nr[3];
-//                float nf[3];
-//                e = grid->_elems[ i ];
-//                if( subVisible[ e->subdomain() ] == false  )
-//                        continue;
-
-//                if (bElements) {
-//                        if( ! visEle.find(i+1) )
-//                                continue;
-//                }
-
-//                cl = subColors[ e->subdomain() ];
-//                glColor4f((GLfloat) cl.red()/255,
-//                                   (GLfloat) cl.green()/255,
-//                                   (GLfloat) cl.blue()/255,
-//                                   fTransparency);
-
-//                if (bElements || e->isExternal() ) {
-//                        for (j=0; j < e->getFacesCount();j++) {
-//                                bDraw=false;
-//                                if (bElements || ((ie = e->getNeighbourWithFaceNr(j)) == 0)) {
-//                                        bDraw = true;
-//                                } else {
-//                                        if (grid->_elems[ ie - 1 ]->subdomain() != e->subdomain())
-//                                                bDraw = true;
-//                                }
-
-//                                if (bDraw) {
-//                                        CFace f;
-//                                        f = e->getFaceOrder( j );
-//                                        f.fill(grid->_nodes); // fill geometrical coordinates of CFace, from nodes vector
-//                                        f.shrink( e->center(), dShrink );
-
-//                                        e->getNormalOfFace( j, nr );
-//                                        SET(nf,nr);
-//                                        VERSOR(nf,nf);
-//                                        if (paintMode == "Solid" || (paintMode == "Elements") )
-//                                                glNormal3f( nf[0], nf[1], nf[2] );
-//                                        for (k = 0; k < 3; k++) {
-//                                                glVertex3f(f.x(k), f.y(k), f.z(k) );
-
-//                                                if (paintMode == "Wireframe") {
-//                                                        if (k<2) {
-//                                                                glVertex3f(f.x(k+1), f.y(k+1), f.z(k+1) );
-//                                                        } else {
-//                                                                glVertex3f(f.x(0), f.y(0), f.z(0) );
-//                                                        }
-//                                                }
-//                                        }
-//                                }
-//                        }
-//                }
+                        normalny4p(points[3].coordinates(), points[0].coordinates(), points[1].coordinates(), points[2].coordinates(), n);
+                        glNormal3f( n[0], n[1], n[2] );
+                        glVertex3f( points[3].x(), points[3].y(), points[3].z() );
+                        glVertex3f( points[0].x(), points[0].y(), points[0].z() );
+                        glVertex3f( points[1].x(), points[1].y(), points[1].z() );
+            }
         }
         glEnd();
 }
@@ -427,14 +354,13 @@ void FVMeshDraw::drawVertices( )
         SetOfInt visVert( getAttrValue(tr("Interesting Vertices")), 1, mesh->num_vertices() );
         float fPointSize = getVertexSize();
 
-        /*
+
         if (visVert.sum() > 1000) {
             if (QMessageBox::question( 0, tr("To many objects warning."), tr("You are trying to draw over 1000 vertices numbers. This process may take a long time. Do you want to continue?"), QMessageBox::Yes | QMessageBox::No ) == QMessageBox::No) {
                 setAttrValue(tr("Show Verts Nums"), "No" );
                 return;
             }
         }
-        */
 
         glEnable(GL_POINT_SMOOTH);
         glPointSize( fPointSize );
@@ -443,11 +369,11 @@ void FVMeshDraw::drawVertices( )
         dolfin::Point p;
 
         for (int i = 0; (unsigned int) i < mesh->num_vertices(); i++) {
-//                if (visVert.find( i+1 )){
+                if (visVert.find( i+1 )){
                     p = mesh->geometry().point(i);
                     glVertex3f( p.x(), p.y(), p.z() );
                     std::cout << p.x() << " " <<p.y() << " " <<p.z() << " \n";
-//                }
+                }
         }
         glEnd();
         glDisable(GL_POINT_SMOOTH);
@@ -477,20 +403,20 @@ int FVMeshDraw::getVertexSize( )
 
 void FVMeshDraw::paintVertsNums( )
 {
-//        SetOfInt visVerts( getAttrValue(tr("Interesting Vertices")), 1, grid->_nodes.size() );
+        SetOfInt visVerts( getAttrValue(tr("Interesting Vertices")), 1, mesh->num_vertices() );
 
-//        for ( int n = 0; n < (int) grid->_nodes.size(); n++) {
-//                if( ! visVerts.find(n+1) )
-//                        continue;
+        for ( int n = 0; n < (int) mesh->num_vertices(); n++) {
+                if( ! visVerts.find(n+1) )
+                        continue;
 
-//                getCurrentViewer()->fvRenderText(grid->_nodes[n]._Coords[0], grid->_nodes[n]._Coords[1], grid->_nodes[n]._Coords[2], QString::number( n+1 ) );
-//        }
+                getCurrentViewer()->fvRenderText(mesh->geometry().point(n).x(), mesh->geometry().point(n).y(), mesh->geometry().point(n).z(), QString::number( n+1 ) );
+        }
 
 }
 
 void FVMeshDraw::setupAttributes( )
 {
-//        grid = reqGrid.getGrid( parentObject(), parent );
+        mesh = reqGrid.getMesh(parentObject(), parent );
 
         qDebug() << "Setting up attributes for " << classType();
         // Here add the attributes
@@ -503,46 +429,37 @@ void FVMeshDraw::setupAttributes( )
         lst.append("Wireframe");
         lst.append("Elements");
         lst.append("Vertices");
-        lst.append("Subdomain wireframe");
+//        lst.append("Subdomain wireframe");
         a->setList( lst );
 
-//        a = am->addAttr( tr("Shrink Elems"), QString("1.0"), "text" );
-//        a = am->addAttr( tr("Vertex Size"), QString("4"), "text" );
-//        a = am->addAttr( tr("Line width"), QString("1.0"), "text" );
+        a = am->addAttr( tr("Shrink Elems"), QString("1.0"), "text" );
+        a = am->addAttr( tr("Vertex Size"), QString("4"), "text" );
+        a = am->addAttr( tr("Line width"), QString("1.0"), "text" );
 
-//        a = am->addAttr( tr("Transparent"), QString("No"), "boolean" );
-//        a = am->addAttr( tr("Transparency Ratio"), fvsettings.value("/RSoft/FViewer/DefTransparencyRatio","0.4").toString(), "text" );
+        a = am->addAttr( tr("Transparent"), QString("No"), "boolean" );
+        a = am->addAttr( tr("Transparency Ratio"), fvsettings.value("/RSoft/FViewer/DefTransparencyRatio","0.4").toString(), "text" );
 
 //        a = am->addSection( tr("Colors") );
-//        a = am->addAttr( tr("Sub.Color Policy"), QString("Single Color"), "textlist" );
-//        lst.clear();
-//        lst.append("Single Color");
-//        lst.append("Individual Colors");
-//        a->setList( lst );
+////        a = am->addAttr( tr("Sub.Color Policy"), QString("Single Color"), "textlist" );
+////        lst.clear();
+////        lst.append("Single Color");
+////        lst.append("Individual Colors");
+////        a->setList( lst );
 
-//        QVariant v;
-//        QColor defColor;
-//        v = fvsettings.value( QString("/RSoft/FViewer/SingleColor"), QVariant(QColor(200,200,200)) );
-//        defColor = v.value<QColor>();
-//        a = am->addAttr( tr("Single Color"), defColor, "color" );
+////        QVariant v;
+////        QColor defColor;
+////        v = fvsettings.value( QString("/RSoft/FViewer/SingleColor"), QVariant(QColor(200,200,200)) );
+////        defColor = v.value<QColor>();
+////        a = am->addAttr( tr("Single Color"), defColor, "color" );
 
-//        if (grid != 0) {
-
-//                for (unsigned int i = 0; i < grid->_subdomains.size(); i++) {
-//                        v = fvsettings.value( QString("/RSoft/FViewer/Color%1").arg(i+1), QVariant(QColor(100,100,100)) );
-//                        defColor = v.value<QColor>();
-//                        a = am->addAttr( tr("Subdomain %1").arg(i+1), defColor, "color" );
-//                }
-
-
-//                a = am->addSection( tr("Visibility") );
+        if (mesh != 0) {
+                a = am->addSection( tr("Visibility") );
 //                a = am->addAttr( tr("Visible Subdomains"), tr("%1-%2").arg(1).arg(grid->_subdomains.size()), "text" );
-//                a = am->addAttr( tr("Show Elems Nums"), QString("No"), "boolean" );
-//                a = am->addAttr( tr("Interesting Elements"), tr("%1-%2").arg(1).arg(grid->getNoElems()), "text" );
-//                a = am->addAttr( tr("Show Verts Nums"), QString("No"), "boolean" );
-//                a = am->addAttr( tr("Interesting Vertices"), tr("%1-%2").arg(1).arg(grid->getNoNodes()), "text" );
-
-//        }
+                a = am->addAttr( tr("Show Elems Nums"), QString("No"), "boolean" );
+                a = am->addAttr( tr("Interesting Elements"), tr("%1-%2").arg(1).arg(mesh->num_entities(3)), "text" );
+                a = am->addAttr( tr("Show Verts Nums"), QString("No"), "boolean" );
+                a = am->addAttr( tr("Interesting Vertices"), tr("%1-%2").arg(1).arg(mesh->num_vertices()), "text" );
+        }
 }
 
 /*
